67351433567299ed0007b7cda43673917f8e26a6
clzola <lazar.radinovic@gmail.com>
2020-01-09 12:56:19 +0100

    Updates theme colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,5 +3,16 @@ import Vuetify from 'vuetify/lib'
 
 Vue.use(Vuetify)
 
+// Slytherin Color Palette: https://www.color-hex.com/color-palette/815
+
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#2a623d',
+        secondary: '#1a472a',
+        accent: '#5d5d5d'
+      }
+    }
+  }
 })
