5c9360f71c3fea0be385cc5d9b718e882f1c51d7
clzola <lazar.radinovic@gmail.com>
2020-01-12 22:36:44 +0100

    Sets brace-style lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'space-before-function-paren': ["error", "never"],
+    'space-before-function-paren': ['error', 'never'],
+    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }]
   },
   parserOptions: {
     parser: 'babel-eslint'
