b8361f22190a33e609359cfb2f55f08674256f63
clzola <lazar.radinovic@gmail.com>
2020-01-15 18:24:00 +0100

    Handles loading and error states

--- src/potionCategories/pages/PotionCategoryPage.vue
+++ src/potionCategories/pages/PotionCategoryPage.vue
@@ -1,6 +1,11 @@
 <template>
   <v-container fluid>
-    <v-row>
+    <v-row v-if="loading">
+      <v-col>
+        <v-progress-linear indeterminate></v-progress-linear>
+      </v-col>
+    </v-row>
+    <v-row v-if="!errorMessage">
       <v-col>
         <v-btn @click="navigateBack">
           <v-icon left>mdi-arrow-left</v-icon>
@@ -8,8 +13,8 @@
         </v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col sm="6" xl="4">
+    <v-row v-if="!loading">
+      <v-col v-if="!errorMessage" sm="6" xl="4">
         <v-card>
           <v-card-text>
             <span class="font-weight-bold">ID:</span>
@@ -21,6 +26,17 @@
           </v-card-text>
         </v-card>
       </v-col>
+      <v-col v-if="errorMessage" sm="12" class="text-center mt-12">
+        <div class="ma-4 red--text">{{ errorMessage }}</div>
+        <v-btn @click="navigateBack">
+          <v-icon left>mdi-arrow-left</v-icon>
+          Back
+        </v-btn>
+        <v-btn class="mx-2" color="blue" dark @click="loadPotionCategory">
+          <v-icon left>mdi-reload</v-icon>
+          Reload
+        </v-btn>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -29,21 +45,32 @@
 export default {
   data() {
     return {
+      loading: true,
       potionCategoryId: this.$route.params.id,
-      potionCategory: {}
+      potionCategory: {},
+      errorMessage: null
     }
   },
   mounted() {
     this.$store.commit('setToolbarTitle', 'Potion Category')
-
-    this.$http.get(`/api/potion-categories/${this.potionCategoryId}`)
-      .then(response => {
-        this.potionCategory = response.data.data
-      })
+    this.loadPotionCategory()
   },
   methods: {
     navigateBack() {
       this.$router.go(-1)
+    },
+    loadPotionCategory() {
+      this.loading = true
+      this.$http
+        .get(`/api/potion-categories/${this.potionCategoryId}`)
+        .then(response => {
+          this.loading = false
+          this.potionCategory = response.data.data
+        })
+        .catch((error) => {
+          this.loading = false
+          this.errorMessage = error.message
+        })
     }
   }
 }
